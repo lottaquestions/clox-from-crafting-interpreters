c8ed4e6e232c0fb509b6cf54b69a12b8e8ae4abb
lottaquestions <nickothieno@yahoo.com>
2023-07-19 06:13:28 -0400

    Up till the end of Chapter 15 - Execution Tracing

--- vm.c
+++ vm.c
@@ -28,6 +28,12 @@ static InterpretResult run()
 {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op) \
+    do{ \
+        double b = pop(); \
+        double a = pop(); \
+        push(a op b); \
+    } while (false)
 
     for (;;)
     {
@@ -51,6 +57,22 @@ static InterpretResult run()
             push(constant);
             break;
         }
+        case OP_ADD:{
+            BINARY_OP(+);
+            break;
+        }
+        case OP_SUBTRACT:{
+            BINARY_OP(-);
+            break;
+        }
+        case OP_MULTIPLY:{
+            BINARY_OP(*);
+            break;
+        }
+        case OP_DIVIDE:{
+            BINARY_OP(/);
+            break;
+        }
         case OP_NEGATE:{
             push(-pop());
             break;
@@ -65,6 +87,7 @@ static InterpretResult run()
     }
 #undef READ_BYTE
 #undef READ_CONSTANT
+#undef BINARY_OP
 }
 
 InterpretResult interpret(Chunk* chunk){
