1afe12400bac29b9cd98010fd6350c2aabadb7e2
lottaquestions <nickothieno@yahoo.com>
2023-08-10 07:13:34 -0400

    Up till the end of Chapter 16 - Scanning on Demand

--- vm.c
+++ vm.c
@@ -1,4 +1,5 @@
 #include "common.h"
+#include "compiler.h"
 #include "debug.h"
 #include "vm.h"
 #include <stdio.h>
@@ -90,9 +91,8 @@ static InterpretResult run()
 #undef BINARY_OP
 }
 
-InterpretResult interpret(Chunk* chunk){
-    vm.chunk = chunk;
-    vm.ip = vm.chunk->code;
-    return run();
+InterpretResult interpret(const char* source){
+    compile(source);
+    return INTERPRET_OK;
 }
 
